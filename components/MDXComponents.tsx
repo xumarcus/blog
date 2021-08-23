3e0e9b4b782bdf13411e36a8a0c9185a10f4acda
xumarcus <xumarcus.sg@gmail.com>
2021-08-23 23:27:18 +0800

    Update first post

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,22 +1,28 @@
-/* eslint-disable react/display-name */
 import React, { useMemo } from 'react'
 import { ComponentMap, getMDXComponent } from 'mdx-bundler/client'
-import Image from './Image'
 import CustomLink from './Link'
-import TOCInline from './TOCInline'
+import Image from './Image'
 import Pre from './Pre'
+import TOCInline from './TOCInline'
+import SimpleLineChart from './SimpleLineChart'
 
-const Wrapper: React.ComponentType<{ layout: string }> = ({ layout, ...rest }) => {
+const Wrapper: React.FunctionComponent<{ layout: string }> = ({ layout, ...rest }) => {
   const Layout = require(`../layouts/${layout}`).default
   return <Layout {...rest} />
 }
 
 export const MDXComponents: ComponentMap = {
-  Image,
-  TOCInline,
+  // Builtins
   a: CustomLink,
+  img: Image,
   pre: Pre,
+  toc: TOCInline,
   wrapper: Wrapper,
+
+  // Custom
+  SimpleLineChart,
+
+  // Library imports
 }
 
 interface Props {
---
+++ components/SimpleLineChart.tsx
@@ -0,0 +1,65 @@
+// Copyright (C) 2021 Marcus Xu
+//
+// This file is part of blog.
+//
+// blog is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// blog is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with blog.  If not, see <http://www.gnu.org/licenses/>.
+
+import {
+  Label,
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  TooltipProps,
+} from 'recharts'
+
+interface Props {
+  data: [string, number][]
+  formatter?: TooltipProps<number, string>['formatter']
+  name: string
+  title?: string
+}
+
+// Ugly hack for `MDXComponents.tsx`
+const SimpleLineChart: React.FunctionComponent<Props> = ({
+  data,
+  formatter,
+  name,
+  title,
+}: Props) => (
+  <>
+    {title && (
+      <ResponsiveContainer width="100%">
+        <strong style={{ display: 'block', textAlign: 'center' }}>{title}</strong>
+      </ResponsiveContainer>
+    )}
+    <ResponsiveContainer aspect={2.0} height="100%" width="100%">
+      <LineChart data={data.map(([x, value]) => ({ x, value }))}>
+        <Label value={title} offset={0} position="insideBottom" />
+        <CartesianGrid strokeDasharray={5} />
+        <XAxis dataKey="x" />
+        <YAxis />
+        <Tooltip formatter={formatter} />
+        <Legend />
+        <Line name={name} type="monotone" dataKey="value" />
+      </LineChart>
+    </ResponsiveContainer>
+  </>
+)
+
+export default SimpleLineChart
